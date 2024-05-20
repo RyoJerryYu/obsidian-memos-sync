dc8373545182b867d33d2e50a19a871f8b2f80a1
Ryo <36154873+RyoJerryYu@users.noreply.github.com>
2024-05-21 00:46:40 +0800

    feat: first produce

--- main.tsx
+++ main.tsx
@@ -1,58 +1,76 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-import { createRoot } from 'react-dom/client'
+import {
+	App,
+	Editor,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+} from "obsidian";
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import { DailyMemos } from "services/DailyMemos";
+import { PluginSettings } from "types/PluginSettings";
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: PluginSettings = {
+	dailyMemosHeader: "Memos",
+	usememosAPI: "https://usememos.com/api/v1",
+	usememosToken: "",
+	attachmentFolder: "Attachments",
+};
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	settings: PluginSettings;
+	dailyMemos: DailyMemos;
 
 	async onload() {
 		await this.loadSettings();
+		await this.loadDailyMemos();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice! Again!2');
-		});
+		const ribbonIconEl = this.addRibbonIcon(
+			"dice",
+			"Sample Plugin",
+			(evt: MouseEvent) => {
+				// Called when the user clicks the icon.
+				new Notice("This is a notice! Again!2");
+			}
+		);
 		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		ribbonIconEl.addClass("my-plugin-ribbon-class");
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		statusBarItemEl.setText("Status Bar Text");
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: "open-sample-modal-simple",
+			name: "Open sample modal (simple)",
 			callback: () => {
 				new SampleModal(this.app).open();
-			}
+			},
 		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: "sample-editor-command",
+			name: "Sample editor command",
 			editorCallback: (editor: Editor, view: MarkdownView) => {
 				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
+				editor.replaceSelection("Sample Editor Command");
+			},
 		});
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
 		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
+			id: "open-sample-modal-complex",
+			name: "Open sample modal (complex)",
 			checkCallback: (checking: boolean) => {
 				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+				const markdownView =
+					this.app.workspace.getActiveViewOfType(MarkdownView);
 				if (markdownView) {
 					// If checking is true, we're simply "checking" if the command can be run.
 					// If checking is false, then we want to actually perform the operation.
@@ -63,33 +81,45 @@ export default class MyPlugin extends Plugin {
 					// This command will only show up in Command Palette when the check function returns true
 					return true;
 				}
-			}
+			},
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
+	onunload() {}
 
-	}
+	loadSettings = async () => {
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
+	};
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
+	saveSettings = async () => {
 		await this.saveData(this.settings);
-	}
+		this.loadDailyMemos()
+	};
+
+	loadDailyMemos = async () => {
+		this.dailyMemos = new DailyMemos(this.app, this.settings);
+		this.addCommand({
+			id: "my-plugin-sync-daily-memos",
+			name: "Sync daily memos",
+			callback: this.dailyMemos.sync,
+		});
+		this.addCommand({
+			id: "my-plugin-force-sync-daily-memos",
+			name: "Force sync daily memos",
+			callback: this.dailyMemos.forceSync,
+		});
+		// timeout
+		// interval
+		// for sync
+		// notice to clear on unload
+	};
 }
 
 class SampleModal extends Modal {
@@ -98,12 +128,12 @@ class SampleModal extends Modal {
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+		contentEl.setText("Woah!");
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
@@ -116,30 +146,68 @@ class SampleSettingTab extends PluginSettingTab {
 		this.plugin = plugin;
 	}
 
+	private saveSettings = (newSettings: Partial<PluginSettings>) => {
+		this.plugin.settings = {
+			...this.plugin.settings,
+			...newSettings,
+		};
+		this.plugin.saveSettings();
+	};
+
 	display(): void {
-		const {containerEl} = this;
+		this.containerEl.empty();
+		this.containerEl.createEl("h3", { text: "Settings for My Plugin." });
 
-		const root = createRoot(containerEl);
-		root.render((
-			<div>
-				<h2>Settings</h2>
-				<p>Here are some settings for the plugin</p>
-				<code>
-					new Setting(containerEl)
-				</code>
-			</div>
+		new Setting(this.containerEl)
+			.setName("Daily Memos Header")
+			.setDesc("The header for the daily memos section.")
+			.addText((textfield) => {
+				textfield.setPlaceholder(DEFAULT_SETTINGS.dailyMemosHeader);
+				textfield.setValue(this.plugin.settings.dailyMemosHeader);
+				textfield.onChange((value) => {
+					this.saveSettings({
+						dailyMemosHeader: value,
+					});
+				});
+			});
 
-		))
+		new Setting(this.containerEl)
+			.setName("Usememos API URL")
+			.setDesc("Usememos API URL.")
+			.addText((textfield) => {
+				textfield.setPlaceholder(DEFAULT_SETTINGS.usememosAPI);
+				textfield.setValue(this.plugin.settings.usememosAPI);
+				textfield.onChange((value) => {
+					this.saveSettings({
+						usememosAPI: value,
+					});
+				});
+			});
 
-		// new Setting(containerEl)
-		// 	.setName('Setting #1')
-		// 	.setDesc('It\'s a secret')
-		// 	.addText(text => text
-		// 		.setPlaceholder('Enter your secret')
-		// 		.setValue(this.plugin.settings.mySetting)
-		// 		.onChange(async (value) => {
-		// 			this.plugin.settings.mySetting = value;
-		// 			await this.plugin.saveSettings();
-		// 		}));
+		new Setting(this.containerEl)
+			.setName("Usememos Token")
+			.setDesc("Usememos token.")
+			.addText((textfield) => {
+				textfield.setPlaceholder(DEFAULT_SETTINGS.usememosToken);
+				textfield.setValue(this.plugin.settings.usememosToken);
+				textfield.onChange((value) => {
+					this.saveSettings({
+						usememosToken: value,
+					});
+				});
+			});
+
+		new Setting(this.containerEl)
+			.setName("Attachment Folder")
+			.setDesc("The folder for attachments.")
+			.addText((textfield) => {
+				textfield.setPlaceholder(DEFAULT_SETTINGS.attachmentFolder);
+				textfield.setValue(this.plugin.settings.attachmentFolder);
+				textfield.onChange((value) => {
+					this.saveSettings({
+						attachmentFolder: value,
+					});
+				});
+			});
 	}
 }
--- services/DailyMemos.ts
+++ services/DailyMemos.ts
@@ -1,3 +1,384 @@
+import axios, { Axios } from "axios";
+import { getAllDailyNotes, getDailyNote } from "obsidian-daily-notes-interface";
+import type { Moment } from "moment";
+import { App, TFile, normalizePath } from "obsidian";
+import { PluginSettings } from "types/PluginSettings";
+import { DailyRecordType, FetchError, ResourceType } from "types/usememos";
+import * as log from "utils/log";
+
+export function generateHeaderRegExp(header: string) {
+	const formattedHeader = /^#+/.test(header.trim())
+		? header.trim()
+		: `# ${header.trim()}`;
+	const reg = new RegExp(`(${formattedHeader}[^\n]*)([\\s\\S]*?)(?=\\n##|$)`);
+
+	return reg;
+}
+
+function generateFileLink(resource: ResourceType): string {
+	if (!resource.externalLink) {
+		return `![[${generateFileName(resource)}]]`;
+	}
+
+	const prefix = resource.type?.includes("image") ? "!" : ""; // only add ! for image type
+
+	return `${prefix}[${resource.name || resource.filename}](${
+		resource.externalLink
+	})`;
+}
+
+function generateFileName(resource: ResourceType): string {
+	return `${resource.id}-${resource.filename.replace(/[/\\?%*:|"<>]/g, "-")}`;
+}
+
+function isBulletList(content: string) {
+	return /^([-*\u2022]|\d+\.) .*/.test(content);
+}
+
+function formatDailyRecord(record: DailyRecordType) {
+	const { createdTs, createdAt, content, resourceList } = record;
+	const timeStamp = createdAt ? window.moment(createdAt).unix() : createdTs;
+	const [date, time] = window.moment(timeStamp * 1000)
+		.format("YYYY-MM-DD HH:mm")
+		.split(" ");
+	const [firstLine, ...otherLine] = content.trim().split("\n");
+	const isTask = /^- \[.*?\]/.test(firstLine); // 目前仅支持 task
+	const isCode = /```/.test(firstLine);
+
+	let targetFirstLine = "";
+
+	if (isTask) {
+		targetFirstLine = `- [ ] ${time} ${firstLine.replace(
+			/^- \[.*?\]/,
+			""
+		)}`;
+	} else if (isCode) {
+		targetFirstLine = `- ${time}`; // 首行不允许存在代码片段
+		otherLine.unshift(firstLine);
+	} else {
+		targetFirstLine = `- ${time} ${firstLine.replace(/^- /, "")}`;
+	}
+
+	targetFirstLine += ` #daily-record ^${timeStamp}`;
+
+	const targetOtherLine = otherLine?.length //剩余行
+		? "\n" +
+		  otherLine
+				.filter((line: string) => line.trim())
+				.map(
+					(line: string) =>
+						`\t${isBulletList(line) ? line : `- ${line}`}`
+				)
+				.join("\n")
+				.trimEnd()
+		: "";
+	const targetResourceLine = resourceList?.length // 资源文件
+		? "\n" +
+		  resourceList
+				?.map(
+					(resource: ResourceType) =>
+						`\t- ${generateFileLink(resource)}`
+				)
+				.join("\n")
+		: "";
+	const finalTargetContent =
+		targetFirstLine + targetOtherLine + targetResourceLine;
+
+	return [date, timeStamp, finalTargetContent].map(String);
+}
+
 export class DailyMemos {
-	constructor() {}
+	private app: App;
+	private settings: PluginSettings;
+	private limit: number;
+	private offset: number;
+	private lastTime: string;
+	private localKey: string;
+	private axios: Axios;
+
+	constructor(app: App, settings: PluginSettings) {
+		if (!settings.usememosAPI) {
+			log.error(
+				"Please set the usememosAPI setting in the plugin settings."
+			);
+			return;
+		}
+
+		this.app = app;
+		this.settings = settings;
+
+		this.localKey = `periodic-para-daily-record-last-time-${this.settings.usememosToken}`;
+		this.limit = 50;
+		this.offset = 0;
+		this.lastTime = window.localStorage.getItem(this.localKey) || "";
+
+		this.axios = axios.create({
+			headers: {
+				Authorization: `Bearer ${this.settings.usememosToken}`,
+				Accept: "application/json",
+			},
+		});
+	}
+
+	/**
+	 * Fetch daily memos from usememos API.
+	 * paginate by {this.limit} and {this.offset}.
+	 */
+	private fetch = async (): Promise<DailyRecordType[] | undefined> => {
+		try {
+			const { data } = await this.axios.get<
+				DailyRecordType[] | FetchError
+			>(this.settings.usememosAPI+`/memo`, {
+				params: {
+					limit: this.limit,
+					offset: this.offset,
+					rowStatus: "NORMAL",
+				},
+			});
+
+			if (Array.isArray(data)) {
+				return data;
+			}
+
+			throw new Error(
+				data.message || data.msg || data.error || JSON.stringify(data)
+			);
+		} catch (error) {
+			log.error(`Failed to fetch daily memos: ${error}`);
+		}
+	};
+
+	forceSync = async () => {
+		this.lastTime = "";
+		this.sync();
+	};
+
+	sync = async () => {
+		log.info("Syncing daily memos...");
+		this.offset = 0;
+		this.downloadResource();
+		this.insertDailyMemos();
+	};
+
+	/**
+	 * Download resources to attachments folder.
+	 * @returns {Promise<void>}
+	 */
+	private downloadResource = async () => {
+		const { origin } = new URL(this.settings.usememosAPI);
+		try {
+			const { data } = await this.axios.get<ResourceType[] | FetchError>(
+				this.settings.usememosAPI +`/resource`
+			);
+
+			if (!Array.isArray(data)) {
+				throw new Error(
+					data.message ||
+						data.msg ||
+						data.error ||
+						JSON.stringify(data)
+				);
+			}
+
+			await Promise.all(
+				data.map(async (resource) => {
+					if (resource.externalLink) {
+						// do not download external resources
+						log.debug(`External resource, skip download: ${resource.externalLink}`)
+						return;
+					}
+					this.settings;
+
+					//TODO: Set the folder use obsidian-daily-notes-interface
+					const folder = this.settings.attachmentFolder;
+					const resourcePath = normalizePath(
+						`${folder}/${generateFileName(resource)}`
+					);
+
+					const isResourceExists =
+						await this.app.vault.adapter.exists(resourcePath);
+					if (isResourceExists) {
+						log.debug(`Resource exists, skip download: ${resourcePath}`);
+						return;
+					}
+
+					const resourceURL = `${origin}/o/r/${
+						resource.uid || resource.name || resource.id
+					}`;
+					const { data } = await this.axios.get(resourceURL, {
+						responseType: "arraybuffer",
+					});
+
+					if (!data) {
+						log.warn(`Failed to fetch resource: ${resourceURL}`);
+						return;
+					}
+
+					//TODO: Create Folder...
+					if (!this.app.vault.getAbstractFileByPath(folder)) {
+						this.app.vault.createFolder(folder);
+					}
+
+					await this.app.vault.adapter.writeBinary(
+						resourcePath,
+						data
+					);
+				})
+			);
+		} catch (error) {
+			if (error.response && error.response.status === 404) {
+				return;
+			}
+			log.error(`Failed to fetch resource: ${error}`);
+		}
+	};
+
+	private insertDailyMemos = async () => {
+		const records = (await this.fetch()) || [];
+
+		const mostRecentRecordTimeStamp = records[0]?.createdAt
+			? window.moment(records[0]?.createdAt).unix()
+			: records[0]?.createdTs;
+
+		if (
+			!records.length ||
+			mostRecentRecordTimeStamp * 1000 < Number(this.lastTime)
+		) {
+			log.info("No new daily memos found.");
+			window.localStorage.setItem(this.localKey, Date.now().toString());
+			return;
+		}
+
+		// generalize daily memos by day and timestamp
+		// map<date, map<timestamp, formattedRecord>>
+		const dailyMemosByDay: Record<string, Record<string, string>> = {};
+		for (const record of records) {
+			if (!record.content && !record.resourceList?.length) {
+				continue;
+			}
+
+			const [date, timestamp, formattedRecord] =
+				formatDailyRecord(record);
+
+			if (dailyMemosByDay[date]) {
+				dailyMemosByDay[date][timestamp] = formattedRecord;
+			} else {
+				dailyMemosByDay[date] = { [timestamp]: formattedRecord };
+			}
+		}
+
+		await Promise.all(
+			Object.entries(dailyMemosByDay).map(
+				async ([today, dailyMemosForToday]) => {
+					const momentDay = window.moment(today);
+
+					//TODO: Set as daily note link
+					const link = `${momentDay.format("YYYY-MM-DD")}.md`;
+
+					//TODO: get file use obsidian-daily-notes-interface
+					// getDailyNote(moment(),getAllDailyNotes())
+					const targetFile =
+						this.app.metadataCache.getFirstLinkpathDest(link, "");
+					// const targetFfile = this.file
+					if (!targetFile) {
+						log.warn(`Failed to find daily note for ${today}`);
+						// TODO create daily note
+						return;
+					}
+
+					// read daily note, modify the memos list
+
+					const originFileContent = await this.app.vault.read(
+						targetFile
+					);
+
+					const modifiedFileContent = await this.modifyDailyNotes(
+						originFileContent,
+						today,
+						dailyMemosForToday
+					);
+					if (!modifiedFileContent) {
+						return;
+					}
+
+					await this.app.vault.modify(
+						targetFile,
+						modifiedFileContent
+					);
+				}
+			)
+		);
+
+		this.offset = this.offset + this.limit;
+		this.insertDailyMemos(); // trailing recursion
+	};
+
+	/**
+	 * Daily Notes will be:
+	 * ```markdown
+	 * contents before
+	 * ...
+	 *
+	 * # The Header
+	 * - memos
+	 * - memos
+	 *
+	 * contents after
+	 * ```
+	 *
+	 * @returns modifiedFileContent
+	 */
+	private modifyDailyNotes = async (
+		originFileContent: string,
+		today: string,
+		fetchedRecordList: Record<string, string>
+	) => {
+		const header = this.settings.dailyMemosHeader;
+		const reg = generateHeaderRegExp(header);
+		const regMatch = originFileContent.match(reg);
+
+		if (!regMatch?.length || !regMatch.index) {
+			log.warn(
+				`Failed to find header for ${today}. Please make sure your daily note template is correct.`
+			);
+			return;
+		}
+
+		const localRecordContent = regMatch[2]?.trim(); // the memos list
+		const from = regMatch.index + regMatch[1].length + 1; // start of the memos list
+		const to = from + localRecordContent.length; // end of the memos list
+		const prefix = originFileContent.slice(0, from); // contents before the memos list
+		const suffix = originFileContent.slice(to); // contents after the memos list
+		const localRecordList = localRecordContent
+			? localRecordContent.split(/\n(?=- )/g)
+			: [];
+
+		// record on memos, has timestamp
+		const existedRecordList: Record<string, string> = {}; // map<timestamp, record>
+
+		for (const record of localRecordList) {
+			const regMatch = record.match(/.*\^(\d{10})/);
+			const createdTs = regMatch?.length ? regMatch[1]?.trim() : "";
+
+			// put records in daily memos into different categories
+			if (createdTs) {
+				existedRecordList[createdTs] = record;
+			}
+		}
+
+		const sortedRecordList = Object.entries({
+			...fetchedRecordList,
+			...existedRecordList,
+		})
+			.sort((a, b) => Number(a[0]) - Number(b[0]))
+			.map((item) => item[1]);
+
+		const modifiedFileContent =
+			prefix.trim() +
+			`\n\n${sortedRecordList}\n\n` +
+			suffix.trim() +
+			`\n`;
+
+		return modifiedFileContent;
+	};
 }
