2db31523936be695e233a8f421d41e82e9b416d7
Ryo <36154873+RyoJerryYu@users.noreply.github.com>
2024-05-23 23:39:08 +0800

    feat: improve daily note modifier

--- services/DailyMemos/DailyMemos.ts
+++ services/DailyMemos/DailyMemos.ts
@@ -12,15 +12,7 @@ import * as log from "utils/log";
 import { MemosClient0191 } from "api/memos-v0.19.1";
 import { generateFileName } from "./memos-util";
 import { MemosPaginator0191 } from "./MemosPaginator";
-
-function generateHeaderRegExp(header: string) {
-	const formattedHeader = /^#+/.test(header.trim())
-		? header.trim()
-		: `# ${header.trim()}`;
-	const reg = new RegExp(`(${formattedHeader}[^\n]*)([\\s\\S]*?)(?=\\n#|$)`);
-
-	return reg;
-}
+import { DailyNoteModifier } from "./DailyNoteModifierRegex";
 
 class DailyNoteManager {
 	private allDailyNotes: Record<string, TFile>;
@@ -198,22 +190,27 @@ export class DailyMemos {
 
 	private insertDailyMemos = async (memosPaginator: MemosPaginator0191) => {
 		const dailyNoteManager = new DailyNoteManager();
+		const dailyNoteModifier = new DailyNoteModifier(
+			this.settings.dailyMemosHeader
+		);
 		const lastTime = await memosPaginator.foreach(
 			async ([today, dailyMemosForToday]) => {
 				const momentDay = window.moment(today);
 
-				const targetFile =
-					await dailyNoteManager.getOrCreateDailyNote(momentDay);
+				const targetFile = await dailyNoteManager.getOrCreateDailyNote(
+					momentDay
+				);
 
 				// read daily note, modify the memos list
 
 				const originFileContent = await this.app.vault.read(targetFile);
 
-				const modifiedFileContent = await this.modifyDailyNotes(
-					originFileContent,
-					today,
-					dailyMemosForToday
-				);
+				const modifiedFileContent =
+					await dailyNoteModifier.modifyDailyNote(
+						originFileContent,
+						today,
+						dailyMemosForToday
+					);
 				if (!modifiedFileContent) {
 					return;
 				}
@@ -224,87 +221,5 @@ export class DailyMemos {
 
 		log.info(`Synced daily memos, lastTime: ${lastTime}`);
 		window.localStorage.setItem(this.localKey, lastTime);
-	};
-
-	/**
-	 * Daily Notes will be:
-	 * ```markdown
-	 * contents before
-	 * ...
-	 *
-	 * # The Header
-	 * - memos
-	 * - memos
-	 *
-	 * contents after
-	 * ```
-	 *
-	 * @returns modifiedFileContent
-	 */
-	private modifyDailyNotes = async (
-		originFileContent: string,
-		today: string,
-		fetchedRecordList: Record<string, string>
-	) => {
-		const header = this.settings.dailyMemosHeader;
-		const reg = generateHeaderRegExp(header);
-		const regMatch = originFileContent.match(reg);
-
-		if (!regMatch?.length || regMatch.index === undefined) {
-			log.debug(`${regMatch}`);
-			log.warn(
-				`Failed to find header for ${today}. Please make sure your daily note template is correct.`
-			);
-			return;
-		}
-
-		const localRecordContent = regMatch[2]?.trim(); // the memos list
-		const from = regMatch.index + regMatch[1].length + 1; // start of the memos list
-		const to = from + localRecordContent.length + 1; // end of the memos list
-		const prefix = originFileContent.slice(0, from); // contents before the memos list
-		const suffix = originFileContent.slice(to); // contents after the memos list
-		const localRecordList = localRecordContent
-			? localRecordContent.split(/\n(?=- )/g)
-			: [];
-
-		// record on memos, has timestamp
-		const existedRecordList: Record<string, string> = {}; // map<timestamp, record>
-
-		for (const record of localRecordList) {
-			const regMatch = record.match(/.*\^(\d{10})/);
-			const createdTs = regMatch?.length ? regMatch[1]?.trim() : "";
-
-			// put records in daily memos into different categories
-			if (createdTs) {
-				existedRecordList[createdTs] = record;
-			}
-		}
-
-		log.debug(
-			`for ${today}\n\nfetchedRecordList: ${JSON.stringify({
-				from,
-				to,
-				prefix,
-				suffix,
-				localRecordList,
-				existedRecordList,
-			})}`
-		);
-
-		const sortedRecordList = Object.entries({
-			...fetchedRecordList,
-			...existedRecordList,
-		})
-			.sort((a, b) => Number(a[0]) - Number(b[0]))
-			.map((item) => item[1])
-			.join("\n");
-
-		const modifiedFileContent =
-			prefix.trim() +
-			`\n\n${sortedRecordList}\n\n` +
-			suffix.trim() +
-			`\n`;
-
-		return modifiedFileContent;
 	};
 }
